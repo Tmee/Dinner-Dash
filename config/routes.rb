4be440a392493a9bfefa74a06a07eb8a6cfe42de
Timothy Mee <tjmee90@gmail.com>
2014-11-18 16:29:20 -0700

    fuck yourself git

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :fillings
 
 
-  get '/',          to: 'users#index'
+  get '/',          to: 'users#home'
   post   '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
