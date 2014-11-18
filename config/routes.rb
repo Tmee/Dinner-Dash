d87d53af3fa235bd4f672a7696112676dbd98be8
Timothy Mee <tjmee90@gmail.com>
2014-11-18 16:19:48 -0700

    merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,9 @@ Rails.application.routes.draw do
   post   '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-<<<<<<< HEAD
-=======
-  get '/', to: 'layouts#application'
+
 
   get 'admin/fillings'
   get 'admin/dashboard'
-
->>>>>>> master
 
 end
