c524280ef71b16a868e2be1732a601827d4e3a1d
Sam Kuhlmann <samkuhlmann@gmail.com>
2014-11-18 05:10:56 -0700

    Add item model and tests

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe Item, :type => :model do
+  let(:item) do 
+    Item.new(title: "Carnitas", 
+             description: "Hot and porky", 
+             price: 3, 
+             food_group: "Meat"
+             )
+  end
+
+  it "is valid" do 
+    expect(item).to be_valid
+  end
+
+  it 'is invalid without a title' do 
+    item.title = nil
+    expect(item).not_to be_valid
+
+    item.title = ""
+    expect(item).not_to be_valid
+  end
+
+  it 'is invalid without a description' do 
+    item.description = nil
+    expect(item).not_to be_valid
+
+    item.description = ""
+    expect(item).not_to be_valid
+  end
+
+  it 'is invalid without a price' do 
+    item.price = nil
+    expect(item).not_to be_valid
+  end
+
+  it 'is invalid without a food group' do 
+    item.food_group = nil
+    expect(item).not_to be_valid
+  end
+
+  it 'must have a price that is a decimal greater than zero' do 
+    item.price = -2
+    expect(item).not_to be_valid
+  end
+
+  it 'cannot create a fan with the same title' do
+    item1 = Item.create(title: "Lettuce", description: "Crispy", price: 1, food_group: "Vegetables")
+    item2 = Item.create(title: "Lettuce", description: "Crispy", price: 1, food_group: "Vegetables")
+
+    expect(item2).not_to be_valid
+  end
+
+  xit 'is associated with a category' do
+    expect(phone_number).to respond_to(:contact)
+  end
+
+end
