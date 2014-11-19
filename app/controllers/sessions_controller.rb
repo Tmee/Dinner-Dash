6c564bfa7670a9859225bcda8323f3195eabe140
Sam Kuhlmann <samkuhlmann@gmail.com>
2014-11-19 10:53:49 -0700

    Fixed login by changing application hmtl form to rails form builder and session controller local user variable

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
+    user = User.find_by(username: params[:username])
 
-    if @user && user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to user_path(user), notice: "Welcome to Los Amigos Gordos, #{user.name}."
+      redirect_to root_path, notice: "Welcome to Los Amigos Gordos, #{user.first_name}."
     else
       redirect_to login_path, notice: "We could not log you in. Please try again."
     end
