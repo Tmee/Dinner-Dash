bac2757c341d5ad08e73c0c1ee0fedb2d07b08aa
Travis Yoder <trayoda@gmail.com>
2014-11-18 22:24:14 -0700

    created a new migration to fix the column names in line_item_fillings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119001444) do
+ActiveRecord::Schema.define(version: 20141119051741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,8 +34,8 @@ ActiveRecord::Schema.define(version: 20141119001444) do
   end
 
   create_table "line_item_fillings", force: true do |t|
-    t.integer  "order_id"
-    t.integer  "item_id"
+    t.integer  "line_item_id"
+    t.integer  "filling_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
