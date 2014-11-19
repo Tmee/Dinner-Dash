eeffe4bdfb1577588cc783279be47035eeacfb4a
Travis Yoder <trayoda@gmail.com>
2014-11-18 22:24:46 -0700

    Updated seed to generate our users table and the admins that go with them
    also generates items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,26 @@
 class Seed
   def initialize
-    generate_users
-    # generate_items
+    generate_users_and_roles
     # generate_orders
+    generate_items
+    # generate_fillings
   end
 
- def generate_users
+ def generate_users_and_roles
+    admin_role     = Role.create!(name: "admin")
+    webmaster_role = Role.create!(name: "webmaster")
+
     User.create!(
       first_name: "Rachel",
       last_name:  "Warbelow",
       email:      "demo+rachel@jumpstartlab.com",
+      username:   "temp",
       password:   "password"
     )
 
     User.create!(
       first_name: "Jeff",
+      last_name:  "temp",
       email:      "demo+jeff@jumpstartlab.com",
       username:   "j3",
       password:   "password"
@@ -28,26 +34,56 @@ class Seed
       password:   "password"
     )
 
-    User.create!(
+    admin_user = User.create!(
       first_name: "Josh",
       last_name:  "Cheek",
       email:      "demo+josh@jumpstartlab.com",
       username:   "josh",
       password:   "password"
     )
-    puts "User #{i}: #{user.name} - #{user.email} created!"
+
+    webmaster_user = User.create!(
+      first_name: "Bruce",
+      last_name:  "Wayne",
+      email:      "Bruce_Wayne@WayneEnterprises.com",
+      username:   "webmaster",
+      password:   "password"
+    )
+
+    admin_user.roles     = [admin_role]
+    webmaster_user.roles = [webmaster_role]
+
+    puts "Users generated!"
   end
 
-  # def generate_items
-  #   500.times do |i|
-  #     filling = Filling.create!(
-  #       name: Faker::Commerce.product_name,
-  #       description: Faker::Lorem.paragraph,
-  #       image_url: "http://robohash.org/#{i}.png?set=set1&size=200x200"
-  #       )
-  #     puts "Filling #{i}: #{filling.name} created!"
-  #   end
-  # end
+  def generate_items
+    Item.create!(
+      name: "Burrito",
+      price: 500
+    )
+
+    Item.create!(
+      name: "Torta",
+      price: 400
+    )
+
+    Item.create!(
+      name: "Taco",
+      price: 100
+    )
+
+    Item.create!(
+      name: "Tamale",
+      price: 200
+    )
+
+    Item.create!(
+      name: "Salad",
+      price: 300
+    )
+
+    puts "Items generated!"
+  end
 
   # def generate_orders
   #   100.times do |i|
