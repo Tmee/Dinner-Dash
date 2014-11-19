6052af2faf073bb6a3514f8552b37090f6cd60c5
Travis Yoder <trayoda@gmail.com>
2014-11-18 17:21:51 -0700

    Add association with admin and changed words to letters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :orders
+  has_many :user_roles
+  has_many :roles, through: :user_roles
 
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :username, length: {minimum: 2, maximum: 32,
-    too_short: "must have at least %{count} words",
-    too_long: "must have at most %{count} words"
+    too_short: "must have at least %{count} letters",
+    too_long: "must have at most %{count} letters"
   }
 end
