51c94e71e19f088bd259410b4cf6d8ff2eef5c2e
Herbert Joseph <techniquea2z@gmail.com>
2014-11-19 09:03:01 -0700

    item/category model/controller work

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,48 +1,48 @@
 class ItemsController < ApplicationController
 
     def index
-      @users = Item.all
+      @items = Item.all
     end
 
     def show
-      @user = Item.find(params[:id])
+      @item = Item.find(params[:id])
     end
 
     def new
-      @user = Item.new
+      @item = Item.new
     end
 
     def edit
-      @user = Item.find(params[:id])
+      @item = Item.find(params[:id])
     end
 
     def create
-      @user = Item.new(user_params)
+      @item = Item.new(item_params)
 
-      if @user.save
-        redirect_to @user, notice: "User created. Please log in."
+      if @item.save
+        redirect_to @item, notice: "Item created. Please log in."
       else
-        flash.now[:notice] = "User could not be created."
+        flash.now[:notice] = "Item could not be created."
         render :new
       end
     end
 
     def update
-      @user = User.find(params[:id])
+      @item = item.find(params[:id])
 
-      if @user.update_attributes(user_params)
-        redirect_to user_path(@user), notice: "You have updated a user."
+      if @item.update_attributes(item_params)
+        redirect_to item_path(@item), notice: "You have updated a product."
       else
-        flash.now[:alert] = "The user was not updated. Please try again."
+        flash.now[:alert] = "The product was not updated. Please try again."
         render :edit
       end
     end
 
     def destroy
-      @user = User.find(params[:id])
+      @item = Item.find(params[:id])
 
-      @user.destroy
-      redirect_to users_path, notice: "The user was deleted."
+      @item.destroy
+      redirect_to items_path, notice: "The product was deleted."
     end
 
     private
