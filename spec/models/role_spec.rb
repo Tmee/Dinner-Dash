0aea0f9c9f3ccbe80fb4cd5e810c783566da97cf
Travis Yoder <trayoda@gmail.com>
2014-11-20 10:21:37 -0700

    Added some test for role.

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Role, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:role) do
+    Role.create(name: "completed")
+  end
+
+  it "is valid" do
+    expect(role).to be_valid
+  end
+
+  it "is valid if no role is provided" do
+    role.name = nil
+    expect(role).to be_valid
+
+    role.name = ""
+    expect(role).to be_valid
+  end
+
 end
