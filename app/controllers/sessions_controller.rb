1b68181b7f4c437d3457cfeecd6844a8d1ff3d31
Timothy Mee <tjmee90@gmail.com>
2014-11-20 23:31:44 -0700

    login displays welcome message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to :back, notice: "Welcome to Los Amigos Gordos, #{user.first_name}."
-    else
+     else
       flash[:error] = true
       redirect_to root_path
     end
