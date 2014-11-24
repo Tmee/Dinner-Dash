55b37b82646b40e9d250a4f811d9fb1e1ae463bc
Herbert Joseph <techniquea2z@gmail.com>
2014-11-24 10:13:22 -0700

    redesigned nav bar with working paths, need to clean login styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141121142705) do
+ActiveRecord::Schema.define(version: 20141124155910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20141121142705) do
     t.integer  "item_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity"
   end
 
   create_table "orders", force: true do |t|
