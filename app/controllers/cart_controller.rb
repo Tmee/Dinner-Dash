1d6d8dc999be83e364cf6c4eb8a6a9522d6ca2cf
Timothy Mee <tjmee90@gmail.com>
2014-11-25 15:40:48 -0700

    added empty funcationality

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,23 +2,34 @@ class CartController < ApplicationController
 
   def index
     @items = Item.all
-    @cart = session[:cart] 
+    @cart = session[:cart]
     @cart_items = Hash.new
+    get_cart_items
+  end
 
-    @cart.each_pair do |key, value|
-      @cart_items[Item.find(key)] = value.map do |filling_id|
-        Filling.find(filling_id)
+  def get_cart_items
+    if cart_nil
+      @cart.each_pair do |key, value|
+        @cart_items[Item.find(key)] = value.map do |filling_id|
+          Filling.find(filling_id)
+        end
       end
+    else
+      redirect_to root_path, notice: "Your cart is empty.  Please fill it up and give us money!"
     end
   end
 
+  def cart_nil
+    @cart.nil? == false
+  end
+
+
   def create
-    session[:cart] ||= {} 
+    session[:cart] ||= {}
     session[:cart][params[:item_id]] = params[:item][:filling_ids]
     redirect_to cart_index_path, notice: "User created. Please log in."
   end
 
   def update
   end
-
 end
