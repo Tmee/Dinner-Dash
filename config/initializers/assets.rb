bab8cf58d0e79d850e574605219ed43ee6b613e4
Herbert Joseph <techniquea2z@gmail.com>
2014-11-24 22:34:25 -0700

    fuck that damn submit button in the form, shittttt

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.precompile += %w( responsive-tables.css )
 Rails.application.config.assets.precompile += %w( responsive-tables.js )
 Rails.application.config.assets.precompile += %w( homepage.css )
 Rails.application.config.assets.precompile += %w( hermano_box.js )
+Rails.application.config.assets.precompile += %w( busborgbuttons.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
