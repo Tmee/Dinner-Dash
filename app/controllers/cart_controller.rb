976f3367ec9037970f7198acdcb557209604be89
Sam Kuhlmann <samkuhlmann@gmail.com>
2014-11-26 09:18:03 -0700

    Add quantity to cart session hash

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,31 +3,41 @@ class CartController < ApplicationController
   def index
     @items = Item.all
     @cart = session[:cart]
-    @cart_items = Hash.new
+    @cart_items = []
     get_cart_items
   end
 
   def get_cart_items
-    if cart_nil
-      @cart.each_pair do |key, value|
-        @cart_items[Item.find(key)] = value.map do |filling_id|
-          Filling.find(filling_id)
-        end
+    if cart_has_items
+      cart_line_items = Hash.new
+      @cart.each do |line_item|
+        cart_line_items[:item_id] = Item.find(line_item["item_id"])
+        cart_line_items[:fillings] = line_item["filling_ids"].map { |filling_id| Filling.find(filling_id) }
+        cart_line_items[:quantity] = line_item["quantity"]
+      @cart_items << cart_line_items
       end
     else
       redirect_to root_path, notice: "Your cart is empty.  Please fill it up and give us money!"
     end
   end
 
-  def cart_nil
+  def cart_has_items
     @cart.nil? == false
   end
 
 
   def create
-    session[:cart] ||= {}
-    session[:cart][params[:item_id]] = params[:item][:filling_ids]
-    redirect_to cart_index_path, notice: "User created. Please log in."
+    session[:cart] ||= []
+    line_item = {"item_id" => params["item_id"], 
+                 "filling_ids" => params["item"]["filling_ids"], 
+                 "quantity" => params["quantity"]
+                }
+    session[:cart] << line_item
+    redirect_to cart_index_path, notice: "Cart created. Please log in."
+
+    # session[:cart] ||= {}
+    # session[:cart][params[:item_id]] = params[:item][:filling_ids]
+    # redirect_to cart_index_path, notice: "User created. Please log in."
   end
 
   def update
