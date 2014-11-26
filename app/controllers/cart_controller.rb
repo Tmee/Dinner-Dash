50645037d8f864f0c48e589055f9b798f3bd0363
Timothy Mee <tjmee90@gmail.com>
2014-11-26 09:57:22 -0700

    saving

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -9,12 +9,13 @@ class CartController < ApplicationController
 
   def get_cart_items
     if cart_has_items
-      cart_line_items = Hash.new
       @cart.each do |line_item|
-        cart_line_items[:item_id] = Item.find(line_item["item_id"])
+        cart_line_items = Hash.new
+        cart_line_items[:item] = Item.find(line_item["item_id"])
         cart_line_items[:fillings] = line_item["filling_ids"].map { |filling_id| Filling.find(filling_id) }
         cart_line_items[:quantity] = line_item["quantity"]
       @cart_items << cart_line_items
+    Rails.logger.debug("tim #{params.inspect}")
       end
     else
       redirect_to root_path, notice: "Your cart is empty.  Please fill it up and give us money!"
@@ -25,16 +26,20 @@ class CartController < ApplicationController
     @cart.nil? == false
   end
 
+  def update_quantity
+
+    redirect_to cart_index_path, notice: "Item quantity has been updated."
+  end
+
 
   def create
     session[:cart] ||= []
-    line_item = {"item_id" => params["item_id"], 
-                 "filling_ids" => params["item"]["filling_ids"], 
+    line_item = {"item_id" => params["item_id"],
+                 "filling_ids" => params["item"]["filling_ids"],
                  "quantity" => params["quantity"]
                 }
     session[:cart] << line_item
     redirect_to cart_index_path, notice: "Cart created. Please log in."
-
     # session[:cart] ||= {}
     # session[:cart][params[:item_id]] = params[:item][:filling_ids]
     # redirect_to cart_index_path, notice: "User created. Please log in."
