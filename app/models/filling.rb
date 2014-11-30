ca74b25ec7fa1c4358c4c3abbf3ebd587890e63b
Travis Yoder <trayoda@gmail.com>
2014-11-29 17:06:00 -0700

    added a line to validate fillings have a retired state

--- app/models/filling.rb
+++ app/models/filling.rb
@@ -2,6 +2,7 @@ class Filling < ActiveRecord::Base
   validates :description, :food_group, presence: true
   validates :title, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { :greater_than_or_equal_to => 0 }
+  validates :retired, inclusion: { in: [true, false] }
   has_many  :line_item_fillings
   has_many  :line_items, through: :line_item_fillings
   has_many  :item_fillings
