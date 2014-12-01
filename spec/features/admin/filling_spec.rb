ae39d9436911c2fcb556225a3a2a727bcd284b8b
Travis Yoder <trayoda@gmail.com>
2014-12-01 11:04:49 -0700

    changed spec back to the way it was before

--- spec/features/admin/filling_spec.rb
+++ spec/features/admin/filling_spec.rb
@@ -5,11 +5,11 @@ feature "Admin can retire a filling" do
 
   before(:each) do
     admin_user = User.create!(first_name: "Josh",
-                              last_name:  "Cheek",
-                              email:      "demo+josh@jumpstartlab.com",
-                              username:   "josh",
-                              password:   "password"
-                             )
+    last_name:  "Cheek",
+    email:      "demo+josh@jumpstartlab.com",
+    username:   "josh",
+    password:   "password"
+    )
     admin = Role.create!(name: "admin")
     admin_user.roles = [admin]
 
@@ -25,6 +25,9 @@ feature "Admin can retire a filling" do
 
   scenario "admin can visit the fillings page" do
     click_link "Admin Dashboard"
+    expect(current_path).to eq("/admin")
+    click_link "Admin Dashboard"
+    expect(current_path).to eq("/admin")
   end
 
 end
