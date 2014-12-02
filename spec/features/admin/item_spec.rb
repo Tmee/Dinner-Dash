b61a8fd3c9ee7e0b44a46f1546c7979de87a3770
Travis Yoder <trayoda@gmail.com>
2014-12-02 13:07:09 -0700

    fixed spacing on the user thats created at the top
    added a filling to the before block so the user
    can interact with it
    
    tried to implement a test for admin making an item

--- spec/features/admin/item_spec.rb
+++ spec/features/admin/item_spec.rb
@@ -3,18 +3,18 @@ require "rails_helper"
 feature "Admin Item Navigation" do
 
   background do
-    admin_user = User.create!(first_name: "Josh",
-    last_name:  "Cheek",
-    email:      "demo+josh@jumpstartlab.com",
-    username:   "josh",
-    password:   "password"
-    )
-    admin = Role.new(name: "admin")
-    admin_user.roles = [admin]
+    admin = User.create!(first_name: "Josh",
+                         last_name:  "Cheek",
+                         email:      "demo+josh@jumpstartlab.com",
+                         username:   "josh",
+                         password:   "password"
+                        )
+    admin_role = Role.new(name: "admin")
+    admin.roles = [admin_role]
 
     visit '/'
-    fill_in "username", with: admin_user.username
-    fill_in "password", with: admin_user.password
+    fill_in "username", with: admin.username
+    fill_in "password", with: admin.password
     find('input[type="submit"]').click
   end
 
@@ -36,16 +36,41 @@ feature "Admin Item Navigation" do
                    description: "It's not a lie. We promise.",
                    price:       700,
                   )
+
+      Filling.create!(title:       "Steak",
+                      description: "It's meat. From an animal.",
+                      price:       100,
+                      food_group:  "meats",
+                     )
     end
 
-    scenario "admin can add a new item" do
+    scenario "admin can go to the new item page" do
       visit "/admin/items"
       click_link "Add New Item"
       expect(page).to have_text("New Item")
+    end
 
+    scenario "admin can make a new item" do
+      visit "/admin/items/new"
+
+      steak_checkbox = find('input[type="checkbox"]')
+      steak_checkbox.set(true)
+      expect(steak_checkbox).to be_checked
+
+      fill_in "Name", with: "Cake"
+      fill_in "Price", with: 100
+
+      click_button "Create Item"
+      # expect(page.status_code).to eq(200)
+      # expect(current_path).to eq(admin_items_path)
+      # expect(Item.last.name).to eq("Cake")
+      # expect(page).to have_text("Cake")
 
     end
+
 
   end
 
 end
+
+# save_and_open_page
