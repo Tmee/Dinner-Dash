c7bc0ea812c856ac7e1c802ac4a2b6f3b182df53
Travis Yoder <trayoda@gmail.com>
2014-12-01 19:58:51 -0700

    finished spec for the admin filling scenarios

--- spec/features/admin/filling_spec.rb
+++ spec/features/admin/filling_spec.rb
@@ -2,20 +2,19 @@ require "rails_helper"
 
 feature "Admin can retire a filling" do
 
-
-  before(:each) do
+  background do
     admin_user = User.create!(first_name: "Josh",
-    last_name:  "Cheek",
-    email:      "demo+josh@jumpstartlab.com",
-    username:   "josh",
-    password:   "password"
-    )
-    admin = Role.create!(name: "admin")
+                              last_name:  "Cheek",
+                              email:      "demo+josh@jumpstartlab.com",
+                              username:   "josh",
+                              password:   "password"
+                             )
+    admin = Role.new(name: "admin")
     admin_user.roles = [admin]
 
     visit '/'
-    fill_in "username", with: "josh"
-    fill_in "password", with: "password"
+    fill_in "username", with: admin_user.username
+    fill_in "password", with: admin_user.password
     find('input[type="submit"]').click
   end
 
@@ -30,12 +29,46 @@ feature "Admin can retire a filling" do
     expect(page).to have_text("Available Ingredients")
   end
 
-  scenario "admin can add a new filling" do
-    visit "/admin/fillings"
-    click_link "Add New Filling"
-    expect(page).to have_text("New Filling")
+  feature "admin can interact with fillings" do
 
+    background do
+      Filling.create!(title:       "Steak",
+                     description: "It's meat. From an animal.",
+                     price:       100,
+                     food_group:  "meats",
+                    )
+    end
 
+    scenario "admin can add a new filling" do
+
+      visit "/admin/fillings"
+      click_link "Add New Filling"
+      expect(page).to have_text("New Filling")
+
+      fill_in "Title",       with: "Cake"
+      fill_in "Description", with: "This is circular food."
+      fill_in "Price",       with: 100
+      fill_in "Food group",  with: "desserts"
+      click_button "Create Filling"
+      expect(page).to have_text("Cake")
+    end
+
+    scenario "admin can edit a filling" do
+      visit "/admin/fillings"
+
+      click_link "Edit"
+      fill_in "Title", with: "Cake"
+      click_button "Update Filling"
+
+      expect(page).to have_text("Cake")
+    end
+
+    scenario "admin can delete a filling" do
+      visit "/admin/fillings"
+      click_link "Destroy"
+
+      expect(page).to have_text("The filling was deleted.")
+    end
   end
 
 end
