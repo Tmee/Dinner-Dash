39bf963d34f4a1e6586d94e6efc0d97bd2f8dd4c
Travis Yoder <trayoda@gmail.com>
2014-12-02 13:00:32 -0700

    Changed words for styling. Removed filling from index since it’s not used.

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,7 +5,6 @@ class Admin::ItemsController < Admin::BaseAdminController
 
     def index
       @items = Item.order(id: :asc)
-      @all_fillings = Filling.order(id: :asc)
     end
 
     def show
