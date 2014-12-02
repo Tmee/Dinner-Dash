75e2e1d4a02b8338b32c9ee69b93a2618796e891
Travis Yoder <trayoda@gmail.com>
2014-12-01 19:59:41 -0700

    removed whitespace

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,29 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Item, :type => :model do
-  let(:item) do 
+  let(:item) do
     Item.new(name: "Burrito",
              price: 500,
              )
   end
 
-  it "is valid" do 
+  it "is valid" do
     expect(item).to be_valid
   end
 
-  it "is invalid without a name" do 
+  it "is invalid without a name" do
     item.name = nil
     expect(item).not_to be_valid
   end
 
-  it 'must have a price that is a decimal greater than zero' do 
+  it 'must have a price that is a decimal greater than zero' do
     item.price = -3
     expect(item).not_to be_valid
   end
 
-  it 'has fillings' do 
+  it 'has fillings' do
     expect(item).to respond_to(:fillings)
   end
-
 
 end
