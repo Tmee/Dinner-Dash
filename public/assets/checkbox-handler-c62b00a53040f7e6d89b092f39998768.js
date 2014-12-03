d3f72b9a057c4e7336704f914c24ba1035f5446d
Timothy Mee <tjmee90@gmail.com>
2014-12-03 10:02:51 -0700

    Fix HTTPS issue, blow away cache

--- public/assets/checkbox-handler-c62b00a53040f7e6d89b092f39998768.js
+++
@@ -1,9 +0,0 @@
-function changeRetiredStatus(event) {
-  var value = $(event.target).prop('checked')
-  $.ajax({
-    url: "/admin/fillings/" + event.target.id,
-    type: "put",
-    data: { filling: { retired: value } }
-  })
-}
-;
